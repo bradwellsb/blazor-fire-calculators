6c6a39e017805eb865d266dfe86e7b2f1ae22236
Bradley Wells <support@wellsb.com>
2019-10-16 17:17:50 +0000

    Making PWA using SQL-MisterMagoo/Blazor.PWA.MSBuild

---
+++ BlazorFireCalculators/wwwroot/ServiceWorker.js
@@ -0,0 +1,116 @@
+const baseURL = '/';
+const indexURL = '/index.html';
+const networkFetchEvent = 'fetch';
+const swInstallEvent = 'install';
+const swInstalledEvent = 'installed';
+const swActivateEvent = 'activate';
+const staticCachePrefix = 'blazor-cache-v';
+const staticCacheName = 'blazor-cache-v1';
+const requiredFiles = [
+"/_framework/blazor.boot.json",
+"/_framework/blazor.server.js",
+"/_framework/blazor.webassembly.js",
+"/_framework/wasm/mono.js",
+"/_framework/wasm/mono.wasm",
+"/_framework/_bin/BlazorFireCalculators.dll",
+"/_framework/_bin/Microsoft.AspNetCore.Authorization.dll",
+"/_framework/_bin/Microsoft.AspNetCore.Blazor.dll",
+"/_framework/_bin/Microsoft.AspNetCore.Blazor.HttpClient.dll",
+"/_framework/_bin/Microsoft.AspNetCore.Components.dll",
+"/_framework/_bin/Microsoft.AspNetCore.Components.Forms.dll",
+"/_framework/_bin/Microsoft.AspNetCore.Components.Web.dll",
+"/_framework/_bin/Microsoft.AspNetCore.Metadata.dll",
+"/_framework/_bin/Microsoft.Bcl.AsyncInterfaces.dll",
+"/_framework/_bin/Microsoft.Extensions.DependencyInjection.Abstractions.dll",
+"/_framework/_bin/Microsoft.Extensions.DependencyInjection.dll",
+"/_framework/_bin/Microsoft.Extensions.Logging.Abstractions.dll",
+"/_framework/_bin/Microsoft.Extensions.Options.dll",
+"/_framework/_bin/Microsoft.Extensions.Primitives.dll",
+"/_framework/_bin/Microsoft.JSInterop.dll",
+"/_framework/_bin/Mono.Security.dll",
+"/_framework/_bin/Mono.WebAssembly.Interop.dll",
+"/_framework/_bin/mscorlib.dll",
+"/_framework/_bin/System.Buffers.dll",
+"/_framework/_bin/System.ComponentModel.Annotations.dll",
+"/_framework/_bin/System.Core.dll",
+"/_framework/_bin/System.dll",
+"/_framework/_bin/System.Memory.dll",
+"/_framework/_bin/System.Net.Http.dll",
+"/_framework/_bin/System.Numerics.Vectors.dll",
+"/_framework/_bin/System.Runtime.CompilerServices.Unsafe.dll",
+"/_framework/_bin/System.Text.Encodings.Web.dll",
+"/_framework/_bin/System.Text.Json.dll",
+"/_framework/_bin/System.Threading.Tasks.Extensions.dll",
+"/css/bootstrap/bootstrap.min.css",
+"/css/bootstrap/bootstrap.min.css.map",
+"/css/open-iconic/FONT-LICENSE",
+"/css/open-iconic/font/css/open-iconic-bootstrap.min.css",
+"/css/open-iconic/font/fonts/open-iconic.eot",
+"/css/open-iconic/font/fonts/open-iconic.otf",
+"/css/open-iconic/font/fonts/open-iconic.svg",
+"/css/open-iconic/font/fonts/open-iconic.ttf",
+"/css/open-iconic/font/fonts/open-iconic.woff",
+"/css/open-iconic/ICON-LICENSE",
+"/css/open-iconic/README.md",
+"/css/site.css",
+"/favicon.ico",
+"/icon-fire192.png",
+"/icon-fire512.png",
+"/index.html",
+"/_redirects",
+"/ServiceWorkerRegister.js",
+"/manifest.json"
+];
+// * listen for the install event and pre-cache anything in filesToCache * //
+self.addEventListener(swInstallEvent, event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(staticCacheName)
+            .then(cache => {
+                return cache.addAll(requiredFiles);
+            })
+    );
+});
+self.addEventListener(swActivateEvent, function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (staticCacheName !== cacheName && cacheName.startsWith(staticCachePrefix)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+self.addEventListener(networkFetchEvent, event => {
+    const requestUrl = new URL(event.request.url);
+    if (requestUrl.origin === location.origin) {
+        if (requestUrl.pathname === baseURL) {
+            event.respondWith(caches.match(indexURL));
+            return;
+        }
+    }
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
+                    .then(response => {
+                        if (response.ok) {
+                            if (requestUrl.origin === location.origin) {
+                                caches.open(staticCacheName).then(cache => {
+                                    cache.put(event.request.url, response);
+                                });
+                            }
+                        }
+                        return response.clone();
+                    });
+            }).catch(error => {
+                console.error(error);
+            })
+    );
+});
