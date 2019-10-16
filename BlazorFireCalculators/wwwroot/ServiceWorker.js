7d32f278165fcfdfbcd604c422f87c0b08acb30a
Bradley Wells <support@wellsb.com>
2019-10-16 22:43:00 +0000

    Use localized currency. Update cache version.

--- BlazorFireCalculators/wwwroot/ServiceWorker.js
+++ BlazorFireCalculators/wwwroot/ServiceWorker.js
@@ -5,7 +5,7 @@ const swInstallEvent = 'install';
 const swInstalledEvent = 'installed';
 const swActivateEvent = 'activate';
 const staticCachePrefix = 'blazor-cache-v';
-const staticCacheName = 'blazor-cache-v1';
+const staticCacheName = 'blazor-cache-v2';
 const requiredFiles = [
 "/_framework/blazor.boot.json",
 "/_framework/blazor.server.js",
