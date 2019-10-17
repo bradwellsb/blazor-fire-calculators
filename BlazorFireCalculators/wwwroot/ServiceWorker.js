467e920ed97bf97c1aaa66be4a5766d050cb1b0e
Bradley Wells <support@wellsb.com>
2019-10-17 22:13:44 +0000

    Fix front-facing typo

--- BlazorFireCalculators/wwwroot/ServiceWorker.js
+++ BlazorFireCalculators/wwwroot/ServiceWorker.js
@@ -5,7 +5,7 @@ const swInstallEvent = 'install';
 const swInstalledEvent = 'installed';
 const swActivateEvent = 'activate';
 const staticCachePrefix = 'blazor-cache-v';
-const staticCacheName = 'blazor-cache-v3';
+const staticCacheName = 'blazor-cache-v4';
 const requiredFiles = [
 "/_framework/blazor.boot.json",
 "/_framework/blazor.server.js",
